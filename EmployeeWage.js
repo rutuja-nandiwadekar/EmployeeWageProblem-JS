e3eee49fd9ce2163d8c0d6879b1ab576ea1a19b1
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-03-31 21:45:34 +0530

    Added: Check epmloyee presence logic

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,2 +1,10 @@
 console.log("*************Welcome to Employee Wage Problem*************");
 
+const IS_PRESENT = 0
+
+let empCheck = Math.floor(Math.random()*10) % 2;
+if (empCheck == IS_PRESENT) {
+    console.log("Employee is present");
+}else{
+    console.log("Employee is absent");
+}
