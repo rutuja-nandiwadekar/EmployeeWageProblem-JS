28a54a0b613253366faab77242309534f6663a7c
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-04-01 15:44:21 +0530

    Added: Function for daily working Hours logic

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,17 +6,18 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOURS = 20;
 
+function getWorkingHours(empCheck){
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
 let empHrs= 0;
 let empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
-}
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOURS;
-console.log("Total Emp Wage: ", empWage)
+console.log("Employee Wage: ", empWage)
