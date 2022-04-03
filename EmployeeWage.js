5740f4a38f41957c927d664c9f5e9f9a57d67409
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-04-03 10:46:34 +0530

    Added: Wage till number of Working Days logic

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,7 +5,8 @@ const IS_FULL_TIME = 2
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOURS = 20;
-const NUM_OF_WORKING_DAYS = 2;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HOURS_IN_MONTH = 160;
 
 function getWorkingHours(empCheck){
     switch (empCheck) {
@@ -18,10 +19,12 @@ function getWorkingHours(empCheck){
     }
 }
 let empHrs= 0;
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++){
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs = getWorkingHours(empCheck);
+let totalWorkingDays = 0;
+while(totalWorkingDays<NUM_OF_WORKING_DAYS && empHrs<=MAX_HOURS_IN_MONTH) {
+    totalWorkingDays++;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs+= getWorkingHours(empCheck);
 }
 
 let empWage = empHrs * WAGE_PER_HOURS;
-console.log("Total working hours: " +empHrs+ "  Total Employee Wage: " +empWage);
+console.log("Total Days: " +totalWorkingDays+ "  Total working hours: " +empHrs+ "  Total Employee Wage: " +empWage);
